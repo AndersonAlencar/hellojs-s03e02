2226ec5d367cff3b24d0489b15c78156d538bb57
AndersonAlencar <anderson.sjav@gmail.com>
2017-09-14 21:31:46 -0300

    Update Index-Axios.js

--- Index-Axios.js
+++ Index-Axios.js
@@ -4,4 +4,4 @@ const api = axios.create({
   baseURL: "https://api.github.com"
 })
 
-api.get("/search/users?q=viniciosbastos").then(ret => console.log(ret.data))
+api.get("/search/users?q=AndersonAlencar").then(ret => console.log(ret.data))
